1d8c34689a68872ad8e7c473700947a5b5005e9d
linan.loheagn3 <linan.loheagn3@bytedance.com>
2023-10-19 15:59:35 +0800

    Initial commit
    Signed-off-by: linan.loheagn3 <linan.loheagn3@bytedance.com>
    Signed-off-by: loheagn <loheagn@icloud.com>

---
+++ folonet-ebpf/src/main.rs
@@ -0,0 +1,68 @@
+#![no_std]
+#![no_main]
+
+use core::mem;
+
+use aya_bpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
+use aya_log_ebpf::info;
+use network_types::{
+    eth::{EthHdr, EtherType},
+    ip::{IpProto, Ipv4Hdr},
+    tcp::TcpHdr,
+    udp::UdpHdr,
+};
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
+
+#[xdp]
+pub fn folonet(ctx: XdpContext) -> u32 {
+    match try_xdp_firewall(ctx) {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_ABORTED,
+    }
+}
+
+#[inline(always)]
+fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start_addr = ctx.data();
+
+    if start_addr + offset + mem::size_of::<T>() > ctx.data_end() {
+        return Err(());
+    }
+
+    Ok((start_addr + offset) as *const T)
+}
+
+fn try_xdp_firewall(ctx: XdpContext) -> Result<u32, ()> {
+    let ethhdr: *const EthHdr = ptr_at(&ctx, 0)?;
+
+    match unsafe { (*ethhdr).ether_type } {
+        EtherType::Ipv4 => {}
+        _ => return Ok(xdp_action::XDP_PASS),
+    }
+
+    let ipv4hdr: *const Ipv4Hdr = ptr_at(&ctx, EthHdr::LEN)?;
+    let source_addr = u32::from_be(unsafe { (*ipv4hdr).src_addr });
+
+    let source_port = match unsafe { (*ipv4hdr).proto } {
+        IpProto::Tcp => {
+            let tcphdr: *const TcpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            u16::from(unsafe { (*tcphdr).source })
+        }
+        IpProto::Udp => {
+            let udphdr: *const UdpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            u16::from(unsafe { (*udphdr).source })
+        }
+        _ => return Err(()),
+    };
+
+    info!(
+        &ctx,
+        "source ip: {:i}, source port: {}", source_addr, source_port
+    );
+
+    Ok(xdp_action::XDP_PASS)
+}
