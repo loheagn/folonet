a8fb303a06032653111104669e6e5a2894db1a92
loheagn <loheagn@icloud.com>
2023-10-26 14:44:43 +0000

    adjust the way to attatch xdp programs to multiple interfaces
    Signed-off-by: loheagn <loheagn@icloud.com>

--- folonet/src/main.rs
+++ folonet/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::{Context, Ok};
+use aya::maps::HashMap;
 use aya::programs::{Xdp, XdpFlags};
 use aya::{include_bytes_aligned, Bpf};
 use aya_log::BpfLogger;
@@ -12,7 +13,10 @@ struct Opt {
     iface: String,
 }
 
-fn attatch_xdp(iface: &str) -> Result<(Bpf, String), anyhow::Error> {
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    env_logger::init();
+
     // Bump the memlock rlimit. This is needed for older kernels that don't use the
     // new memcg based accounting, see https://lwn.net/Articles/837122/
     let rlim = libc::rlimit {
@@ -29,39 +33,34 @@ fn attatch_xdp(iface: &str) -> Result<(Bpf, String), anyhow::Error> {
     // like to specify the eBPF program at runtime rather than at compile-time, you can
     // reach for `Bpf::load_file` instead.
     #[cfg(debug_assertions)]
-    let  bpf = Bpf::load(include_bytes_aligned!(
+    let mut bpf = Bpf::load(include_bytes_aligned!(
         "../../target/bpfel-unknown-none/debug/folonet"
     ))?;
     #[cfg(not(debug_assertions))]
-    let  bpf = Bpf::load(include_bytes_aligned!(
+    let mut bpf = Bpf::load(include_bytes_aligned!(
         "../../target/bpfel-unknown-none/release/folonet"
     ))?;
 
-    Ok((bpf, iface.into()))
-}
+    if let Err(e) = BpfLogger::init(&mut bpf) {
+        // This can happen if you remove all log statements from your eBPF program.
+        warn!("failed to initialize eBPF logger: {}", e);
+    }
 
-#[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
-    env_logger::init();
+    // init maps
+    let mut conn_map: HashMap<_, u32, u32> =
+        HashMap::try_from(bpf.map_mut("CONNECTION").unwrap()).unwrap();
+    conn_map.insert(8, 9, 0)?;
 
-    let iface_list = ["lima0", "lima1", "eth0"];
-    let mut bpf_list: Vec<(Bpf, String)> = iface_list
-        .iter()
-        .map(|iface| attatch_xdp(iface).unwrap())
-        .collect();
-    bpf_list.iter_mut().for_each(|(bpf, iface)| { 
-        let opt = Opt{
-            iface: iface.clone(),
+    let program: &mut Xdp = bpf.program_mut("folonet").unwrap().try_into().unwrap();
+    program.load().unwrap();
+
+    let iface_list = ["lima0", "lima1"];
+    iface_list.iter().for_each(|iface| {
+        let opt = Opt {
+            iface: iface.to_string(),
         };
-        if let Err(e) = BpfLogger::init(bpf) {
-            // This can happen if you remove all log statements from your eBPF program.
-            warn!("failed to initialize eBPF logger: {}", e);
-        }
-
-        let program: &mut Xdp = bpf.program_mut("folonet").unwrap().try_into().unwrap();
-        program.load().unwrap();
         program.attach(&opt.iface, XdpFlags::default())
-            .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE").unwrap();
+                .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE").unwrap();
     });
 
     info!("Waiting for Ctrl-C...");
