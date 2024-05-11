b393d3020b624c8acd11dadd50b88e889c56fafd
loheagn <loheagn@icloud.com>
2024-05-11 15:33:40 +0000

    enable to control servers automatically
    Signed-off-by: loheagn <loheagn@icloud.com>

--- folonet-client/src/lib.rs
+++ folonet-client/src/lib.rs
@@ -13,30 +13,29 @@ async fn get_server_manager_client() -> ServerManagerClient<Channel> {
     //     .connect()
     //     .await
     //     .unwrap();
-    ServerManagerClient::connect("http://[::1]:50051")
+    ServerManagerClient::connect("http://[::1]:7788")
         .await
         .unwrap()
 }
 
 pub async fn start_server(local_endpoint: String) -> Option<config::ServiceConfig> {
     let mut client = get_server_manager_client().await;
-    let server_endpoint = client
+    let server = client
         .start_server(Request::new(StartServerRequest {
             local_endpoint: local_endpoint.clone(),
         }))
         .await
         .unwrap()
-        .into_inner()
-        .server_endpoint;
+        .into_inner();
 
-    if server_endpoint == local_endpoint {
+    if !server.active {
         return None;
     }
 
     Some(config::ServiceConfig {
-        name: "nginx".to_string(),
+        name: server.name.clone(),
         local_endpoint: local_endpoint.clone(),
-        servers: vec![server_endpoint],
+        servers: vec![server.server_endpoint.clone()],
         is_tcp: true,
     })
 }
--- folonet-server/main.go
+++ folonet-server/main.go
@@ -2,26 +2,82 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"net/http"
+	"sync"
+	"sync/atomic"
 
 	pb "github.com/loheagn/folonet/folonet-server/folonetrpc"
 	"google.golang.org/grpc"
 )
+
+type ServerUnit struct {
+	name     string
+	remoteIP string
+}
+
+var record map[string]ServerUnit
+var recordMutex sync.Mutex
+
+var localPort int32 = 8000
+
+func getLocalPort() int32 {
+	return atomic.AddInt32(&localPort, 1)
+}
+
+var localIP = "10.251.254.100"
 
 type server struct {
 	pb.UnimplementedServerManagerServer
 }
 
 func (s *server) StartServer(ctx context.Context, in *pb.StartServerRequest) (*pb.StartServerResponse, error) {
-	if in.LocalEndpoint == "192.168.105.2:80" {
-		return &pb.StartServerResponse{ServerEndpoint: "192.168.105.3:80"}, nil
+	recordMutex.Lock()
+	server, ok := record[in.LocalEndpoint]
+	recordMutex.Unlock()
+
+	if !ok {
+		return &pb.StartServerResponse{Active: false}, nil
 	}
-	return &pb.StartServerResponse{ServerEndpoint: in.LocalEndpoint}, nil
+
+	return &pb.StartServerResponse{
+		Active:         true,
+		ServerEndpoint: server.remoteIP,
+		Name:           server.name,
+	}, nil
+}
+
+func registry(w http.ResponseWriter, r *http.Request) {
+	// 解析查询参数
+	query := r.URL.Query()
+	name := query.Get("name")
+	ip := query.Get("ip")
+
+	// 打印接收到的参数（可选，用于调试）
+	fmt.Printf("Received: name=%s, ip=%s\n", name, ip)
+
+	localPort := getLocalPort()
+
+	localEndpoint := fmt.Sprintf("%s:%d", localIP, localPort)
+
+	recordMutex.Lock()
+	record[localEndpoint] = ServerUnit{name: name, remoteIP: ip}
+	recordMutex.Unlock()
+
+	// 返回响应
+	w.Write([]byte(localEndpoint))
 }
 
 func main() {
-	lis, err := net.Listen("tcp", ":50051")
+	record = make(map[string]ServerUnit)
+	go func() {
+		http.HandleFunc("/registry", registry)
+		http.ListenAndServe(":7777", nil)
+	}()
+
+	lis, err := net.Listen("tcp", ":7788")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
